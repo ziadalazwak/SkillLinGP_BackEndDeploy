a34de300e15c8eb1d8425fd31b618dbd8b26388e
ziad <ziodnaser33@gmail.com>
2026-05-12 04:38:00 +0300

    fixcors

--- SkillLink/SkillLink.API/Program.cs
+++ SkillLink/SkillLink.API/Program.cs
@@ -39,7 +39,7 @@ builder.Services.AddCors(options =>
     options.AddPolicy("AllowFrontend", policy =>
     {
         policy
-            .WithOrigins(builder.Configuration["FrontendUrl:Docker"])
+            .WithOrigins(builder.Configuration["FrontendUrl:local"])
             .AllowAnyHeader()
             .AllowAnyMethod().AllowCredentials(); 
     });
